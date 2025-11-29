b79e378e65bcc6db60d6db883365c6e736176097
Usman Shafeeq <usmanshafeeqit@gmail.com>
2025-11-30 00:57:14 +0500

    fix: Add ProductDetails route to navbar navigation
    - Added 'Details' link to navbar pointing to /product-details
    - Makes ProductDetails page easily accessible from navigation
    - Mobile menu also includes the new link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar() {
   const navLinks = [
     { name: "Home", path: "/" },
     { name: "Products", path: "/products" },
+    { name: "Details", path: "/product-details" },
     { name: "Cart", path: "/cart" },
   ];
 
