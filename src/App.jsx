6c00816a1df1d78b7fe99dcddb028c510b34afa6
Usman Shafeeq <usmanshafeeqit@gmail.com>
2025-11-30 00:43:27 +0500

    feat: Add ProductDetails component and update routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,21 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import Products from "./components/Products";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
-  console.log("✅ App is working correctly!");
+  console.log("App is working correctly!");
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/products" element={<Products />} />
-        <Route path="*" element={<div className="text-center p-12">Page Not Found</div>} />
+        <Route path="/product-details" element={<ProductDetails />} />
+        <Route
+          path="*"
+          element={<div className="text-center p-12">Page Not Found</div>}
+        />
       </Routes>
     </Router>
   );
---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductDetails() {
+  return (
+    <div>
+      <h1 className='center bg-amber-400'>ProductDetails</h1>
+    </div>
+  );
+}
